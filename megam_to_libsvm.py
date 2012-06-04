50c334c92f24b05e17fbcbf7aa63c05d087a9fa1
Dan Blanchard <dblanchard@ets.org>
2012-06-04 17:17:42 +0000

    - Made megam_to_libsvm accessible via Python API. - Added ablility to use liblinear instead of MegaM for the modeling to crossvalidate_megam.py.

--- megam_to_libsvm.py
+++ megam_to_libsvm.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
-# Script that converts MegaM files to ARFF format
+# Script that converts MegaM files to LibSVM format
 
-# Author: Dan Blanchard, dblanchard@ets.org, Oct 2011
+# Author: Dan Blanchard, dblanchard@ets.org, May 2012
 
 import argparse
 import sys
 from collections import defaultdict
 from itertools import izip
 from operator import itemgetter
+
 
 # Globals
 NUM_FIELDS = 0
@@ -27,6 +28,41 @@ def get_next_class_num():
     return NUM_CLASSES
 
 
+def convert_to_libsvm(lines):
+    '''
+        Converts a sequence of lines (e.g., a file or list of strings) in MegaM format to LibSVM format.
+        @param lines: The sequence of lines to convert.
+        @type lines: L{file} or L{list} of L{str}
+
+        @return: A tuple of the newly formatted data, the mappings from class names to numbers, and the mappings from feature names to numbers.
+        @rtype: 3-L{tuple} of (L{list} of L{str}, L{dict}, and L{dict})
+    '''
+    global NUM_FIELDS, NUM_CLASSES
+
+    # Initialize variables
+    NUM_FIELDS = 0
+    NUM_CLASSES = 0
+    field_num_dict = defaultdict(get_next_field_num)
+    class_num_dict = defaultdict(get_next_class_num)
+
+    result_list = []
+
+    # Iterate through MegaM file
+    for line in lines:
+        result_string = ''
+        split_line = line.strip().split()
+        if len(split_line) > 1:
+            result_string += str(class_num_dict[split_line[0]])
+            del split_line[0]
+            # Loop through all feature-value pairs printing out pairs separated by commas (and with feature names replaced with numbers)
+            for field_num, value in sorted(izip([field_num_dict[field_name] for field_name in split_line[::2]], [float(value) for value in split_line[1::2]])):
+                if float(value):
+                    result_string += ' {}:{}'.format(field_num, value)
+            result_list.append(result_string)
+
+    return result_list, class_num_dict, field_num_dict
+
+
 if __name__ == '__main__':
     # Get command line arguments
     parser = argparse.ArgumentParser(description="Takes a MegaM-compatible file to be run with the '-fvals' switch and outputs a " +
@@ -39,21 +75,11 @@ if __name__ == '__main__':
     if args.infile.isatty():
         print >> sys.stderr, "You are running this script interactively. Press CTRL-D at the start of a blank line to signal the end of your input. For help, run it with --help\n"
 
-    # Initialize variables
-    field_num_dict = defaultdict(get_next_field_num)
-    class_num_dict = defaultdict(get_next_class_num)
+    line_list, class_num_dict, field_num_dict = convert_to_libsvm(args.infile)
 
-    # Iterate through MegaM file
-    for line in args.infile:
-        split_line = line.strip().split()
-        if len(split_line) > 1:
-            print class_num_dict[split_line[0]],
-            del split_line[0]
-            # Loop through all feature-value pairs printing out pairs separated by commas (and with feature names replaced with numbers)
-            for field_num, value in sorted(izip([field_num_dict[field_name] for field_name in split_line[::2]], [float(value) for value in split_line[1::2]])):
-                if float(value):
-                    sys.stdout.write(' {}:{}'.format(field_num, value))
-            print
+    # Iterate through converted MegaM file
+    for line in line_list:
+        print line
 
     # Print out mappings to file
     print >> args.mappingfile, "CLASS NUM\tCLASS NAME"
