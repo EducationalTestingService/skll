c461c950521e6e1e845178321759e16d3b200cff
Dan Blanchard <dblanchard@ets.org>
2012-07-31 16:28:28 +0000

    - Made it properly handle sparse MegaM files by using default value of 0 for any missing features.

--- megam_to_arff.py
+++ megam_to_arff.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
+'''
+Script that converts MegaM files to ARFF format
 
-# Script that converts MegaM files to ARFF format
+@author: Dan Blanchard, dblanchard@ets.org
+@date: Oct 2011
+'''
 
-# Author: Dan Blanchard, dblanchard@ets.org, Oct 2011
+from __future__ import print_function, unicode_literals
 
 import argparse
+import csv
 import sys
+
+from bs4 import UnicodeDammit
 
 
 if __name__ == '__main__':
@@ -19,16 +26,20 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.infile.isatty():
-        print >> sys.stderr, "You are running this script interactively. Press CTRL-D at the start of a blank line to signal the end of your input. For help, run it with --help\n"
+        print("You are running this script interactively. Press CTRL-D at the start of a blank line to signal the end of your input. For help, run it with --help\n", 
+              file=sys.stderr)
 
     # Initialize variables
     classes = set()
     instances = []
-    fields = []
+    fields = set()
 
     # Iterate through MegaM file
     first = True
     for line in args.infile:
+        # Process encoding
+        line = UnicodeDammit(line, ['utf-8', 'windows-1252']).unicode_markup
+
         # Ignore comments
         if not line.startswith('#'):
             split_line = line.strip().split()
@@ -36,22 +47,29 @@ if __name__ == '__main__':
                 class_name = split_line[0]
                 classes.add(class_name)
                 field_pairs = split_line[1:]
-                # Get names of all fields if this is the first row we've processed
-                if first:
-                    fields = field_pairs[::2]
-                    first = False
+                # Add field names to set in case there are new ones
+                field_names = field_pairs[::2]
+                field_values = field_pairs[1::2]
+                fields.update(field_names)
                 # Add all the field values (and the current class value) to the list of instances
-                instances.append(field_pairs[1::2] + [class_name])
+                instances.append(dict(zip(field_names, field_values) + [(args.classname, class_name)]))
 
     # Add relation to header
-    print "@relation '{}'\n".format(args.relation)
+    print("@relation '{}'\n".format(args.relation))
 
     # Loop through fields writing the header info for the ARFF file
-    for field in fields:
-        print "@attribute '{}' numeric".format(field.replace('\\', '\\\\').replace("'", "\\'"))
-    print "@attribute {} ".format(args.classname) + "{" + ','.join([str(x) for x in classes]) + "}"
+    sorted_fields = sorted(fields)
+    for field in sorted_fields:
+        print("@attribute '{}' numeric".format(field.replace('\\', '\\\\').replace("'", "\\'")))
+    print("@attribute {} ".format(args.classname) + "{" + ','.join([unicode(x) for x in classes]) + "}")
 
-    print "\n@data"
+    # Create CSV writer to handle missing values for lines in data section
+    csv.excel.lineterminator = '\n'
+    csv.unregister_dialect('excel')
+    csv.register_dialect('excel', csv.excel)
+    writer = csv.DictWriter(sys.stdout, sorted_fields + [args.classname], restval=0)
+
+    print("\n@data")
     # Loop through the list of instances, writing the ARFF file
     for instance in instances:
-        print ','.join([str(x) for x in instance])
+        writer.writerow(instance)
