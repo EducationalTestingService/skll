1f17b24c6a86ae494014cb3462ec3a0afa1aaddd
Dan Blanchard <dblanchard@ets.org>
2012-11-19 09:50:36 -0500

    Added UniqueNumberDict class and cleaned up hacky stuff with globals for number fields and classes.

--- megam_to_libsvm.py
+++ megam_to_libsvm.py
@@ -10,30 +10,19 @@ from __future__ import print_function, unicode_literals
 
 import argparse
 import sys
-from collections import defaultdict
 from itertools import islice, izip
 from operator import itemgetter
 
 from bs4 import UnicodeDammit
 
 
-# Globals
-NUM_FIELDS = 0
-NUM_CLASSES = 0
+class UniqueNumberDict(dict):
+    """ Class for creating sequential unique numbers for each key."""
 
-
-def get_next_field_num():
-    ''' Returns the next available field number '''
-    global NUM_FIELDS
-    NUM_FIELDS += 1
-    return NUM_FIELDS
-
-
-def get_next_class_num():
-    ''' Returns the next available class number '''
-    global NUM_CLASSES
-    NUM_CLASSES += 1
-    return NUM_CLASSES
+    def __getitem__(self, key):
+        if key not in self:
+            self[key] = len(self)
+        return dict.__getitem__(self, key)
 
 
 def convert_to_libsvm(lines):
@@ -45,13 +34,10 @@ def convert_to_libsvm(lines):
         @return: A tuple of the newly formatted data, the mappings from class names to numbers, and the mappings from feature names to numbers.
         @rtype: 3-L{tuple} of (L{list} of L{unicode}, L{dict}, and L{dict})
     '''
-    global NUM_FIELDS, NUM_CLASSES
 
     # Initialize variables
-    NUM_FIELDS = 0
-    NUM_CLASSES = 0
-    field_num_dict = defaultdict(get_next_field_num)
-    class_num_dict = defaultdict(get_next_class_num)
+    field_num_dict = UniqueNumberDict()
+    class_num_dict = UniqueNumberDict()
 
     result_list = []
     # Iterate through MegaM file
@@ -82,6 +68,49 @@ def convert_to_libsvm(lines):
             result_list.append(result_string)
 
     return result_list, class_num_dict, field_num_dict
+
+
+def convert_to_libsvm_iter(lines, class_num_dict=None, field_num_dict=None):
+    '''
+        Iterator-based version of convert_to_libsvm.
+        Converts a sequence of lines (e.g., a file or list of strings) in MegaM format to LibSVM format.
+        @param lines: The sequence of lines to convert.
+        @type lines: L{file} or L{list} of L{str}
+
+        @return: A tuple of the newly formatted data, the mappings from class names to numbers, and the mappings from feature names to numbers.
+        @rtype: 2-L{tuple} of (L{list} of L{unicode} and L{dict})
+    '''
+
+    # Initialize variables
+    field_num_dict = field_num_dict if field_num_dict else UniqueNumberDict()
+    class_num_dict = class_num_dict if class_num_dict else UniqueNumberDict()
+
+    # Iterate through MegaM file
+    for line in lines:
+        line_fields = set()
+        # Process encoding
+        line = UnicodeDammit(line, ['utf-8', 'windows-1252']).unicode_markup.strip()
+
+        # Ignore comments (and TEST/DEV lines)
+        if not line.startswith('#') and not line == 'TEST' and not line == 'DEV':
+            result_string = ''
+            split_line = line.split()
+            result_string += unicode(class_num_dict[split_line[0]])
+            # Handle features if there are any
+            if len(split_line) > 1:
+                del split_line[0]
+                # Loop through all feature-value pairs printing out pairs separated by commas (and with feature names replaced with numbers)
+                for field_num, value in sorted(izip((field_num_dict[field_name] for field_name in islice(split_line, 0, None, 2)),
+                                                    (float(value) if value != 'N/A' else 0.0 for value in islice(split_line, 1, None, 2)))):
+                    # Check for duplicates
+                    if field_num in line_fields:
+                        field_name = (field_name for field_name, f_num in field_num_dict.items() if f_num == field_num).next()
+                        raise AssertionError("Field {} occurs on same line twice.".format(field_name))
+                    # Otherwise output non-empty features
+                    elif value != 'N/A' and float(value):
+                        result_string += ' {}:{}'.format(field_num, value)
+                        line_fields.add(field_num)
+            yield result_string, class_num_dict, field_num_dict
 
 
 def main():
