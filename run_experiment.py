4bf1fdb83bc22293145de8729fa4b3ebd52a5dba
Dan Blanchard <dblanchard@ets.org>
2013-01-17 11:21:22 -0500

    Renamed evaluate.py because it can do lots of different things (like just generate predictions).

---
+++ run_experiment.py
@@ -0,0 +1,298 @@
+#!/usr/bin/env python
+'''
+Runs a bunch of sklearn jobs in parallel on the cluster given a config file.
+
+@author: Nitin Madnani, nmadnani@ets.org
+@author: Dan Blanchard, dblanchard@ets.org
+'''
+
+from __future__ import print_function, unicode_literals
+
+import argparse
+import ConfigParser
+import re
+import os
+import sys
+from collections import defaultdict, namedtuple
+from itertools import chain
+
+import classifier
+from pythongrid import Job, process_jobs
+from texttable import Texttable
+
+# Named tuple for storing job results
+ClassifierResultInfo = namedtuple('ClassifierResultInfo', ['train_set_name', 'test_set_name', 'featureset', 'given_classifier', 'task', 'task_results'])
+
+
+def clean_path(path):
+    ''' Replace all weird SAN paths with normal paths '''
+
+    path = re.sub(r'/\.automount/\w+/SAN/NLP/(\w+)-(dynamic|static)', r'/home/nlp-\1/\2', path)
+    path = re.sub(r'/\.automount/[^/]+/SAN/Research/HomeResearch', '/home/research', path)
+    return path
+
+
+def get_stat_string(class_result_dict, stat):
+    ''' Little helper for getting output for precision, recall, and f-score columns in confusion matrix. '''
+    if stat in class_result_dict and class_result_dict[stat]:
+        return "{:.1f}%".format(class_result_dict[stat] * 100)
+    else:
+        return "N/A"
+
+
+def print_fancy_output(result_tuples, output_file=sys.stdout):
+    ''' Function to take all of the results from all of the folds and print nice tables with the resluts. '''
+    num_folds = len(result_tuples)
+    score_sum = 0.0
+    prec_sum_dict = defaultdict(float)
+    recall_sum_dict = defaultdict(float)
+    f_sum_dict = defaultdict(float)
+    classes = sorted(result_tuples[0][2].iterkeys())
+    folds_with_class = defaultdict(int)
+    for k, (conf_matrix, fold_score, result_dict) in enumerate(result_tuples, start=1):
+        if num_folds > 1:
+            print("\nFold: {}".format(k), file=output_file)
+        result_table = Texttable(max_width=0)
+        result_table.set_cols_align(["r"] * (len(classes) + 4))
+        result_table.add_rows([[""] + classes + ["Precision", "Recall", "F-measure"]], header=True)
+        for i, actual_class in enumerate(classes):
+            conf_matrix[i][i] = "[{}]".format(conf_matrix[i][i])
+            class_prec = get_stat_string(result_dict[actual_class], "Precision")
+            class_recall = get_stat_string(result_dict[actual_class], "Recall")
+            class_f = get_stat_string(result_dict[actual_class], "F-measure")
+            if class_prec != 'N/A':
+                folds_with_class[actual_class] += 1
+                prec_sum_dict[actual_class] += float(class_prec[:-1])
+                recall_sum_dict[actual_class] += float(class_recall[:-1])
+                f_sum_dict[actual_class] += float(class_f[:-1])
+                result_table.add_row([actual_class] + conf_matrix[i] + [class_prec, class_recall, class_f])
+        print(result_table.draw(), file=output_file)
+        print("(row = reference; column = predicted)", file=output_file)
+        print("Accuracy = {:.1f}%\n".format(fold_score), file=output_file)
+        score_sum += fold_score
+
+    if num_folds > 1:
+        print("\nAverage:", file=output_file)
+        result_table = Texttable(max_width=0)
+        result_table.set_cols_align(["l", "r", "r", "r"])
+        result_table.add_rows([["Class", "Precision", "Recall", "F-measure"]], header=True)
+        for actual_class in classes:
+            if folds_with_class[actual_class]:
+                result_table.add_row([actual_class] + ["{:.1f}%".format(prec_sum_dict[actual_class] / folds_with_class[actual_class]),
+                                                       "{:.1f}%".format(recall_sum_dict[actual_class] / folds_with_class[actual_class]),
+                                                       "{:.1f}%".format(f_sum_dict[actual_class] / folds_with_class[actual_class])])
+        print(result_table.draw(), file=output_file)
+        print("Accuracy = {:.1f}%".format(score_sum / num_folds), file=output_file)
+
+
+def classify_featureset(featureset, given_classifiers, train_path, test_path, train_set_name, test_set_name, modelpath, vocabpath, prediction_prefix, grid_search,
+                        grid_objective, cross_validate, evaluate, suffix, log_path):
+    ''' Classification job to be submitted to grid '''
+    result_list = []
+
+    # initialize a classifer object
+    learner = classifier.Classifier()
+
+    with open(log_path, 'w') as log_file:
+        if cross_validate:
+            print("Cross-validating on {}, feature set {} ...".format(train_set_name, featureset), file=log_file)
+        else:
+            print("Training on {}, Test on {}, feature set {} ...".format(train_set_name, test_set_name, featureset), file=log_file)
+
+        # tunable parameters for each model type
+        tunable_parameters = {'dtree': ['max_depth', 'max_features'], \
+                              'svm_linear': ['C'], \
+                              'svm_radial': ['C', 'gamma'], \
+                              'logistic': ['C'], \
+                              'naivebayes': ['alpha'], \
+                              'rforest': ['max_depth', 'max_features']}
+
+        # load the training and test examples
+        train_examples = classifier.load_examples(os.path.join(train_path, featureset + suffix))
+        if not cross_validate:
+            test_examples = classifier.load_examples(os.path.join(test_path, featureset + suffix))
+
+        # the name of the feature vocab file
+        vocabfile = os.path.join(vocabpath, '{}.vocab'.format(featureset))
+
+        # load the feature vocab if it already exists. We can do this since this is independent of the model type
+        if os.path.exists(vocabfile):
+            print('\tloading pre-existing feature vocab', file=log_file)
+            learner.load_vocab(vocabfile)
+
+        # now go over each classifier
+        for given_classifier in given_classifiers:
+            # set the model type
+            learner.model_type = given_classifier
+
+            # check whether a trained model on the same data with the same featureset already exists
+            # if so, load it (and the feature vocabulary) and then use it on the test data
+            modelfile = os.path.join(modelpath, given_classifier, '{}.model'.format(featureset))
+
+            # load the model if it already exists
+            if os.path.exists(modelfile):
+                print('\tloading pre-existing {} model'.format(given_classifier), file=log_file)
+                learner.load_model(modelfile)
+
+            # if we have do not have a saved model, we need to train one. However, we may be able to reuse a saved feature vocab file if that existed above.
+            else:
+                if learner.feat_vectorizer:
+                    print('\ttraining new {} model'.format(given_classifier), file=log_file)
+                    best_score = learner.train(train_examples, grid_search=grid_search, grid_objective=grid_objective)
+                else:
+                    print('\tfeaturizing and training new {} model'.format(given_classifier), file=log_file)
+                    best_score = learner.train(train_examples, grid_search=grid_search, grid_objective=grid_objective)
+
+                    # save vocab
+                    learner.save_vocab(vocabfile)
+
+                # save model
+                learner.save_model(modelfile)
+
+                # print out the tuned parameters and best CV score
+                if grid_search:
+                    param_out = []
+                    for param_name in tunable_parameters[given_classifier]:
+                        param_out.append('{}: {}'.format(param_name, learner.model.get_params()[param_name]))
+                    print('\ttuned hyperparameters: {}'.format(', '.join(param_out)), file=log_file)
+                    print('\tbest score: {}'.format(round(best_score, 3)), file=log_file)
+
+            # run on test set or cross-validate on training data, depending on what was asked for
+            if cross_validate:
+                print('\tcross-validating', file=log_file)
+                results = learner.cross_validate(train_examples, prediction_prefix=prediction_prefix)
+                task = 'cross-validate'
+            elif evaluate:
+                print('\tevaluating predictions', file=log_file)
+                results = [learner.evaluate(test_examples, prediction_prefix=prediction_prefix)]
+                task = 'evaluate'
+            else:
+                print('\twriting predictions', file=log_file)
+                task = 'predict'
+                learner.predict(test_examples, prediction_prefix)
+                continue
+
+            # write out the tsv row to STDOUT
+            result_list.append(ClassifierResultInfo(train_set_name, test_set_name, featureset, given_classifier, task, results))
+
+    return result_list
+
+
+def run_configuration(config_file):
+    ''' Takes a configuration file and runs the specified jobs on the grid. '''
+    # initialize config parser
+    configurator = ConfigParser.RawConfigParser({'test_location': '', 'results': '', 'predictions': '', "grid_search": False, 'objective': "f1_score_micro"})
+    configurator.read(config_file)
+
+    # extract sklearn parameters from the config file
+    given_classifiers = eval(configurator.get('Input', 'classifiers'))
+    given_featuresets = eval(configurator.get("Input", "featuresets"))
+
+    # get all the input paths and directories
+    train_path = configurator.get("Input", "train_location").rstrip('/')  # remove trailing / at the end of path name
+    test_path = configurator.get("Input", "test_location").rstrip('/')
+    suffix = configurator.get("Input", "suffix")
+
+    # get all the output files and directories
+    resultsfile = configurator.get("Output", "results")
+    resultspath = os.path.dirname(resultsfile) if resultsfile else ""
+    logfile = configurator.get("Output", "log")
+    logpath = os.path.dirname(logfile)
+    modelpath = configurator.get("Output", "models")
+    vocabpath = configurator.get("Output", "vocabs")
+
+    # create the path of the resultsfile, logfile and the modelpath
+    os.system("mkdir -p {} {}".format(resultspath, logpath))
+
+    # do we want to keep the predictions?
+    prediction_prefix = configurator.get("Output", "predictions")
+    if prediction_prefix:
+        predictdir = os.path.dirname(prediction_prefix)
+        os.system("mkdir -p {}".format(predictdir))
+
+    # make sure all the specified paths exist
+    if not (os.path.exists(train_path) and (not test_path or os.path.exists(test_path))):
+        print("Error: the training and/or test path(s) specified in config file does not exist.", file=sys.stderr)
+        sys.exit(2)
+
+    # make sure all the given classifiers are valid as well
+    if set(given_classifiers).difference(set(['dtree', 'svm_linear', 'svm_radial', 'logistic', 'naivebayes', 'rforest'])):
+        print("Error: unrecognized classifier in config file.", file=sys.stderr)
+        sys.exit(2)
+
+    # do we need to run a grid search for the hyperparameters or are we just using the defaults
+    do_grid_search = eval(configurator.get("Tuning", "grid_search"))
+
+    # what is the objective function for the grid search?
+    grid_objective_func = configurator.get("Tuning", "objective")
+    if grid_objective_func not in ['f1_score_micro', 'f1_score_macro', 'accuracy']:
+        print('Error: invalid grid objective function.', file=sys.stderr)
+        sys.exit(2)
+    else:
+        grid_objective_func = 'classifier.' + grid_objective_func
+
+    # are we doing cross validation or actual testing or just generating predictions on a new test set?
+    # If no test set was specified then assume that we are doing cross validation.
+    # If the results field was not specified then assume that we are just generating predictions
+    evaluate = False
+    cross_validate = False
+    predict = False
+    if test_path and resultspath:
+        evaluate = True
+    elif not test_path:
+        cross_validate = True
+    else:
+        predict = True
+
+    # make sure that, if we are in prediction mode,we have a prediction_prefix
+    if predict and not prediction_prefix:
+        print('Error: you need to specify a prediction prefix if you are using prediction mode (no "results" option in config file).', file=sys.stderr)
+        sys.exit(2)
+
+
+    # the list of jobs submitted
+    jobs = []
+
+    # For each feature set
+    for featureset in given_featuresets:
+        # store training/test set names for later use
+        train_set_name = os.path.basename(train_path)
+        test_set_name = os.path.basename(test_path) if test_path else "cv"
+
+        # create a name for the job
+        jobname = 'run_{}_{}_{}'.format(train_set_name, test_set_name, featureset)
+
+        # change the prediction prefix to include the feature set
+        featset_prediction_prefix = prediction_prefix + '-' + featureset.replace('+', '-')
+
+        # the log file
+        temp_logfile = os.path.join(logpath, '{}.log'.format(jobname))
+
+        # create job
+        job = Job(classify_featureset, [featureset, given_classifiers, train_path, test_path, train_set_name, test_set_name,
+                                        modelpath, vocabpath, featset_prediction_prefix, do_grid_search, eval(grid_objective_func), cross_validate,
+                                        evaluate, suffix, temp_logfile], num_slots=(5 if do_grid_search else 1), name=jobname)
+
+        # Add job to list
+        jobs.append(job)
+
+    # submit the jobs
+    job_results = process_jobs(jobs)
+
+    # Print out results
+    for result_info in chain.from_iterable(job_results):
+        if result_info.task != 'predict':
+            with open(os.path.join(resultspath, '{}_{}_{}_{}_{}.results'.format(result_info.train_set_name, result_info.test_set_name, result_info.featureset,
+                                                                                result_info.given_classifier, result_info.task)), 'w') as output_file:
+                print_fancy_output(result_info.task_results, output_file)
+
+
+if __name__ == '__main__':
+    # Get command line arguments
+    parser = argparse.ArgumentParser(description="Runs a bunch of sklearn jobs in parallel on the cluster given a config file.",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                     conflict_handler='resolve')
+    parser.add_argument('config_file', help='Configuration file describing the sklearn task to run.')
+    args = parser.parse_args()
+
+    run_configuration(args.config_file)
