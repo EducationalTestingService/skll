91ba7afd5cfc08119786f8b47cc9727bb29a9fd4
Dan Blanchard <dblanchard@ets.org>
2013-06-26 12:19:04 -0400

    Added option to only include instances that are common to all MegaM files that were passed in. Also made code a little more PEP8 compliant.

--- join_megam.py
+++ join_megam.py
@@ -27,8 +27,12 @@ def parse_num_list(num_string):
         Convert a string representing a range of numbers to a list of integers.
     '''
     range_list = []
-    if (num_string != '') and (not re.match(r'^(\d+(-\d+)?,)*\d+(-\d+)?$', num_string)):
-        raise argparse.ArgumentTypeError("'" + num_string + "' is not a range of numbers. Expected forms are '8-15', '4,8,15,16,23,42', or '8-15,42'.")
+    if (num_string != '') and (not re.match(r'^(\d+(-\d+)?,)*\d+(-\d+)?$',
+                                            num_string)):
+        raise argparse.ArgumentTypeError("'" + num_string + "' is not a range" +
+                                         " of numbers. Expected forms are " +
+                                         "'8-15', '4,8,15,16,23,42', or " +
+                                         "'8-15,42'.")
     for rng in num_string.split(','):
         if rng.count('-'):
             split_range = [int(x) for x in rng.split('-')]
@@ -51,7 +55,10 @@ def get_unique_name(feature_name, prev_feature_set, filename):
     while new_feature_name in prev_feature_set:
         new_feature_name += "_" + suffix
     if new_feature_name != feature_name and feature_name not in warned_about:
-        print("Warning: Feature named {} already found in previous files. Renaming to {} to prevent duplicates.".format(feature_name, new_feature_name).encode('utf-8'),
+        print(("Warning: Feature named {} already found in previous files. " +
+               "Renaming to {} to prevent " +
+               "duplicates.").format(feature_name,
+                                     new_feature_name).encode('utf-8'),
               file=sys.stderr)
         warned_about.add(feature_name)
     return new_feature_name
@@ -59,20 +66,39 @@ def get_unique_name(feature_name, prev_feature_set, filename):
 
 if __name__ == '__main__':
     # Get command line arguments
-    parser = argparse.ArgumentParser(description="Combine MegaM files that contain features for the same files.",
+    parser = argparse.ArgumentParser(description="Combine MegaM files that \
+                                                  contain features for the same\
+                                                  files.",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('megam_file', help='MegaM input file(s). Each feature line must be preceded by a comment with the filename/ID ' +
-                                           'that the features should be joined on.', type=argparse.FileType('r'), nargs='+')
-    parser.add_argument('-b', '--binary', help='Converts all of the features in the specified range of files to presence/absence binary features. Files are numbered ' +
-                                               'starting from 1, and if 0 is specified with this flag, all files are converted.', type=parse_num_list)
-    parser.add_argument('--doubleup', help='Keep both the binary and numeric versions of any feature you convert to binary.', action='store_true')
+    parser.add_argument('megam_file',
+                        help='MegaM input file(s). Each feature line must be \
+                              preceded by a comment with the filename/ID that \
+                              the features should be joined on.',
+                        type=argparse.FileType('r'), nargs='+')
+    parser.add_argument('-b', '--binary',
+                        help='Converts all of the features in the specified \
+                              range of files to presence/absence binary \
+                              features. Files are numbered starting from 1, and\
+                              if 0 is specified with this flag, all files are\
+                              converted.',
+                        type=parse_num_list)
+    parser.add_argument('--doubleup',
+                        help='Keep both the binary and numeric versions of any\
+                              feature you convert to binary.',
+                        action='store_true')
+    parser.add_argument('-c', '--common',
+                        help='Only output features for filenames that are \
+                              common to all MegaM files.',
+                        action='store_true')
     args = parser.parse_args()
 
     # Map from filenames to feature strings
     feature_dict = OrderedDict()
     class_dict = {}
+    filename_set = set()
 
-    # Set that will contain all of the features seen in previous files (for duplicate detection)
+    # Set that will contain all of the features seen in previous files
+    # (for duplicate detection)
     prev_feature_set = set()
 
     # Iterate through MegaM files
@@ -80,14 +106,21 @@ if __name__ == '__main__':
         # Initialize duplicate feature book-keeping variables
         curr_feature_set = set()
 
+        # Initialize set for storing filenames mentioned in current MegaM file
+        curr_filename_set = set()
+
         # Handle current MegaM file
-        print("Loading {}...".format(infile.name).encode('utf-8'), file=sys.stderr)
+        print("Loading {}...".format(infile.name).encode('utf-8'),
+              file=sys.stderr)
         sys.stderr.flush()
         for line in infile:
-            stripped_line = UnicodeDammit(line.strip(), ['utf-8', 'windows-1252']).unicode_markup
+            stripped_line = UnicodeDammit(line.strip(),
+                                          ['utf-8',
+                                           'windows-1252']).unicode_markup
             # Read current filename from comment
             if stripped_line.startswith('#'):
                 curr_filename = stripped_line.lstrip('# ')
+                curr_filename_set.add(curr_filename)
             # Ignore TEST and DEV lines and store features
             elif stripped_line not in ['TEST', 'DEV']:
                 split_line = stripped_line.split('\t', 1)
@@ -97,12 +130,16 @@ if __name__ == '__main__':
                     feature_pairs = split_line[1].split(' ')
                     feature_names = feature_pairs[0::2]
                     if len(feature_names) != len(set(feature_names)):
-                        print("Error: Duplicate features occur on the line of features for {}.".format(curr_filename).encode('utf-8'), file=sys.stderr)
+                        print(("Error: Duplicate features occur on the line of" +
+                               "features for {}.").format(curr_filename).encode('utf-8'),
+                              file=sys.stderr)
                         sys.exit(1)
                     feature_values = islice(feature_pairs, 1, None, 2)
-                    for feat_name, feat_val in izip(feature_names, feature_values):
+                    for feat_name, feat_val in izip(feature_names,
+                                                    feature_values):
                         # Handle duplicate features
-                        feat_name = get_unique_name(feat_name, prev_feature_set, infile.name)
+                        feat_name = get_unique_name(feat_name, prev_feature_set,
+                                                    infile.name)
                         # Ignore zero-valued features
                         try:
                             if feat_val != 'N/A' and float(feat_val) != 0:
@@ -116,23 +153,42 @@ if __name__ == '__main__':
                                     feat_val = 1
 
                                 # Add feature pair to current string of features
-                                new_feat_pair = '{} {} '.format(feat_name, feat_val)
+                                new_feat_pair = '{} {} '.format(feat_name,
+                                                                feat_val)
                                 feature_dict[curr_filename] = new_feat_pair if curr_filename not in feature_dict else feature_dict[curr_filename] + new_feat_pair
                                 curr_feature_set.add(feat_name)
                         except ValueError:
-                            print("Error: Invalid feature value in feature pair '{} {}' for file {}".format(feat_name, feat_val, curr_filename).encode('utf-8'), file=sys.stderr)
+                            print(("Error: Invalid feature value in feature " +
+                                   "pair '{} {}' for file {}").format(feat_name,
+                                                                      feat_val,
+                                                                      curr_filename).encode('utf-8'),
+                                  file=sys.stderr)
                             sys.exit(1)
 
-                # Otherwise warn about lack of features (although that really just means all of them have zero values)
+                # Otherwise warn about lack of features (although that really
+                # just means all of them have zero values)
                 else:
                     if curr_filename not in feature_dict:
                         feature_dict[curr_filename] = ""
-                    print("Warning: No features found for {} in {}. All are assumed to be zero.".format(curr_filename, infile.name).encode('utf-8'), file=sys.stderr)
+                    print(("Warning: No features found for {} in {}. All are " +
+                           "assumed to be zero.").format(curr_filename,
+                                                         infile.name).encode('utf-8'),
+                          file=sys.stderr)
 
         # Add current file's features to set of seen features
         prev_feature_set.update(curr_feature_set)
 
+        # Either intersect or union current file's filenames with existing ones
+        if args.common and filename_set:
+            filename_set.intersection_update(curr_filename_set)
+        else:
+            filename_set.update(curr_filename_set)
+
     # Print new MegaM file
     for curr_filename in feature_dict.viewkeys():
+        # Skip files that aren't common when args.common is true
+        if curr_filename not in filename_set:
+            continue
         print("# {}".format(curr_filename).encode('utf-8'))
-        print("{}\t{}".format(class_dict[curr_filename], feature_dict[curr_filename].strip()).encode('utf-8'))
+        print("{}\t{}".format(class_dict[curr_filename],
+                              feature_dict[curr_filename].strip()).encode('utf-8'))
