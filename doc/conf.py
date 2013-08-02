601c953116c01fd2b040e8a54a305f46f0f35e53
Dan Blanchard <dblanchard@ets.org>
2013-08-02 10:53:44 -0400

    Changed theme so that read the docs will use default.

--- doc/conf.py
+++ doc/conf.py
@@ -94,8 +94,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "armstrong"
-html_theme_path = ["_themes", ]
+#html_theme = "armstrong"
+#html_theme_path = ["_themes", ]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
