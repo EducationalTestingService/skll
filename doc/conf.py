6f0cf33e0917d2e1c0b49b552a2ddc23733d1fda
Dan Blanchard <dblanchard@ets.org>
2013-08-02 11:04:34 -0400

    Change theme based on whether or not we're on RTD.

--- doc/conf.py
+++ doc/conf.py
@@ -94,8 +94,12 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = "armstrong"
-#html_theme_path = ["_themes", ]
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+    html_theme = 'default'
+else:
+    html_theme = 'armstrong'
+    html_theme_path = ["_themes", ]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
