4e86c2be0b31e84b7bf62215a7e361c0fd930cbf
Dan Blanchard <dblanchard@ets.org>
2013-11-01 10:01:37 -0400

    Fix unit test I broke when fixing #62.

--- tests/test_skll.py
+++ tests/test_skll.py
@@ -383,10 +383,10 @@ def test_summary():
         reader = csv.DictReader(f, dialect='excel-tab')
 
         for row in reader:
-            # the learner results dictionaries should have 17 rows,
+            # the learner results dictionaries should have 18 rows,
             # and all of these except results_table
             # should be printed (though some columns will be blank).
-            eq_(len(row), 17)
+            eq_(len(row), 18)
             assert row['model_params']
             assert row['grid_score']
             assert row['score']
