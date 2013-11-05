5417222663f32dc274a270014976e3cb9c3e5c7e
Dan Blanchard <dblanchard@ets.org>
2013-11-05 10:23:22 -0500

    Made multiple examples and cleaned up make_example_iris_data a little bit.

--- examples/make_example_iris_data.py
+++ examples/make_example_iris_data.py
@@ -12,12 +12,21 @@ from __future__ import print_function, unicode_literals
 
 import json
 import os
+import sys
+
 import sklearn.datasets
 from sklearn.cross_validation import train_test_split
 
 
 def main():
+    '''
+    Download some example data and split it into training and test data.
+    '''
+    print('Retrieving iris data from servers...', end='')
     iris_data = sklearn.datasets.load_iris()
+    print('done')
+    sys.stdout.flush()
+
 
     X = iris_data['data']
     Y = [iris_data['target_names'][label] for label in iris_data['target']]
@@ -30,6 +39,7 @@ def main():
     examples_train, examples_test = train_test_split(examples, test_size=0.33,
                                                      random_state=42)
 
+    print('Writing training and testing files...', end='')
     for examples, suffix in [(examples_train, 'train'), (examples_test,
                                                          'test')]:
         iris_dir = os.path.join('iris', suffix)
@@ -40,6 +50,8 @@ def main():
         with open(jsonlines_path, 'w') as f:
             for ex in examples:
                 f.write('{}\n'.format(json.dumps(ex)))
+    print('done')
+
 
 if __name__ == '__main__':
     main()
