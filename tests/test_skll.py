32259d58a376832de5821cecb71035dc938191d7
Chong Min Lee <clee001@ets.org>
2014-10-17 14:00:19 -0400

    updated 19 to 20 after getting unit test failures

--- tests/test_skll.py
+++ tests/test_skll.py
@@ -553,10 +553,10 @@ def test_summary_feature_hasher():
         reader = csv.DictReader(f, dialect='excel-tab')
 
         for row in reader:
-            # the learner results dictionaries should have 19 rows,
+            # the learner results dictionaries should have 20 rows,
             # and all of these except results_table
             # should be printed (though some columns will be blank).
-            eq_(len(row), 19)
+            eq_(len(row), 20)
             assert row['model_params']
             assert row['grid_score']
             assert row['score']
@@ -606,10 +606,10 @@ def test_summary():
         reader = csv.DictReader(f, dialect='excel-tab')
 
         for row in reader:
-            # the learner results dictionaries should have 19 rows,
+            # the learner results dictionaries should have 20 rows,
             # and all of these except results_table
             # should be printed (though some columns will be blank).
-            eq_(len(row), 19)
+            eq_(len(row), 20)
             assert row['model_params']
             assert row['grid_score']
             assert row['score']
