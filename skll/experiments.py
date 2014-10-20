0b8b18ce4a469e46383ea70b82dfcb55e009bdfc
Keelan Evanini <kevanini@ets.org>
2014-10-20 08:45:06 -0400

    Removed check for whether test_file is set when checking set-up for 'evaluate' and 'cross_validate' conditions; since test_file is set to test_path earlier, this check can be removed.

--- skll/experiments.py
+++ skll/experiments.py
@@ -446,10 +446,10 @@ def _parse_config_file(config_path):
                           '{}').format(grid_objective))
 
     # check whether the right things are set for the given task
-    if (task == 'evaluate' or task == 'predict') and not (test_path or test_file):
+    if (task == 'evaluate' or task == 'predict') and not (test_path):
         raise ValueError('The test set must be set when task is evaluate'
                          ' or predict.')
-    if (task == 'cross_validate' or task == 'train') and (test_path or test_file):
+    if (task == 'cross_validate' or task == 'train') and (test_path):
         raise ValueError('The test set should not be set ' +
                          'when task is cross_validate or train.')
     if (task == 'train' or task == 'predict') and results_path:
