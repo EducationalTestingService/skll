6877d48ad5785a0cdd950abf00a09fee657d6648
Nitin Madnani <nmadnani@ets.org>
2014-10-31 21:12:00 -0400

    Fix bug in featureset __eq__
    - array.{data,indices} are not always in the same order so since we are comparing them for equality we first need to sort the indices.

--- skll/data/featureset.py
+++ skll/data/featureset.py
@@ -91,14 +91,16 @@ class FeatureSet(object):
         equal if any differences are in the sixth
         decimal place or higher.
         '''
+        f1 = self.features.sorted_indices()
+        f2 = other.features.sorted_indices()
         return (self.ids.shape == other.ids.shape and
                 self.classes.shape == other.classes.shape and
-                self.features.shape == other.features.shape and
+                f1.shape == f2.shape and
                 (self.ids == other.ids).all() and
                 (self.classes == other.classes).all() and
-                np.allclose(self.features.data, other.features.data, rtol=1e-6) and
-                (self.features.indices == other.features.indices).all() and
-                (self.features.indptr == other.features.indptr).all() and
+                np.allclose(f1.data, f2.data, rtol=1e-6) and
+                (f1.indices == f2.indices).all() and
+                (f1.indptr == f2.indptr).all() and
                 self.vectorizer == other.vectorizer)
 
 
