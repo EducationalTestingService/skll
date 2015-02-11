6630c7c02ab22cdc832f2515d46d356d1ef857f7
Mark Chan <mkchan@gmail.com>
2015-02-11 14:03:41 -0800

    Added KMeans unsupervised classifier

--- skll/learner.py
+++ skll/learner.py
@@ -43,6 +43,7 @@ from sklearn.kernel_approximation import (AdditiveChi2Sampler, Nystroem,
 from sklearn.linear_model import (ElasticNet, Lasso, LinearRegression,
                                   LogisticRegression, Ridge, SGDClassifier,
                                   SGDRegressor)
+from sklearn.cluster import (KMeans)
 from sklearn.linear_model.base import LinearModel
 from sklearn.metrics import (accuracy_score, confusion_matrix,
                              precision_recall_fscore_support, SCORERS)
@@ -74,6 +75,8 @@ _DEFAULT_PARAM_GRIDS = {AdaBoostClassifier:
                         [{'max_depth': [1, 3, 5]}],
                         GradientBoostingRegressor:
                         [{'max_depth': [1, 3, 5]}],
+                        KMeans:
+                        [{}],
                         KNeighborsClassifier:
                         [{'n_neighbors': [1, 5, 10, 100],
                           'weights': ['uniform', 'distance']}],
