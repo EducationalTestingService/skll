c167d533b18b757ebf1beafba31ad6c90c9f9ef2
Aoife Cahill <acahill@ets.org>
2016-02-12 16:23:18 -0500

    change to warning

--- skll/data/featureset.py
+++ skll/data/featureset.py
@@ -24,8 +24,8 @@ class FeatureSet(object):
     """
     Encapsulation of all of the features, values, and metadata about a given
     set of data.
-    WARNING: FeatureSets can only be equal if the order of
-    the instances is identical because these are stored as lists/arrays.
+    warning:: FeatureSets can only be equal if the order of the instances is
+    identical because these are stored as lists/arrays.
 
     This replaces ``ExamplesTuple`` from older versions.
 
