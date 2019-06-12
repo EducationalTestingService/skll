b7a8a485603e4b92ddd92425fd55e88ee09ae10a
Robert Pugh <robert.pugh@coursehero.com>
2019-06-12 10:25:00 -0700

    add tests for both classifiers and regressors to verify that  parameters aren't duplicated in default param grids

--- tests/test_classification.py
+++ tests/test_classification.py
@@ -124,6 +124,18 @@ def check_predict(model, use_feature_hashing=False):
     eq_(len(predictions), test_fs.features.shape[0])
 
 
+def test_default_classifier_param_grids_no_duplicates():
+    """
+    Verify that the default parameter grids for classifiers don't contain
+    duplicate values.
+    """
+    for learner, param_list in _DEFAULT_PARAM_GRIDS.items():
+        if learner.endswith('Classifier'):
+            param_dict = param_list[0]
+            for param_name, values in param_dict.items():
+                assert(len(set(values)) == len(values))
+
+
 # the runner function for the prediction tests
 def test_predict():
     for model, use_feature_hashing in \
