2bb41ecd278eefe47df2b5a8d23d62a4c9bdf04c
jbiggsets <jbiggs@ets.org>
2019-10-14 14:24:12 -0400

    This commit includes the following updates: - Check the JSON results in test_examples - Add the expected JSON files

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -5,14 +5,18 @@ Run the examples, just to make sure they are all still working
 :author: Jeremy Biggs (jbiggs@@ets.org)
 """
 
+import json
+
 from glob import glob
 from os import chdir, getcwd, listdir, makedirs
-from os.path import abspath, basename, dirname, join
+from os.path import abspath, basename, dirname, exists, join
 from shutil import copyfile, rmtree
 
 from examples import make_titanic_example_data as mtd
 from examples import make_boston_example_data as mbd
 from examples import make_iris_example_data as mid
+
+from nose.tools import eq_, assert_almost_equal
 
 from skll.experiments import run_configuration
 
@@ -38,6 +42,43 @@ def run_function_in_custom_dir(func, old_dir, new_dir):
     chdir(new_dir)
     func()
     chdir(old_dir)
+
+
+def run_configuration_and_check_outputs(config_path):
+    """
+    Run the configuration, and then check the JSON results
+    against expected JSON files
+    """
+
+    # run this experiment, get the `results_json_path`
+    results_json_path = run_configuration(config_path, local=True, quiet=True)[0]
+
+    # if the results path exists, check the output
+    if exists(results_json_path):
+
+        results_json_exp_path = join(_my_dir, 'other', 'expected', basename(results_json_path))
+        results_obj = json.load(open(results_json_path, 'r'))[0]
+        results_exp_obj = json.load(open(results_json_exp_path, 'r'))[0]
+
+        # we check a subset of the values, just to make sure
+        # that nothing weird is going on with our output
+        for key in ["train_set_size", "test_set_size",
+                    "learner_name", "cv_folds", "feature_scaling",
+                    "grid_score", "grid_objective",
+                    "accuracy", "score", "pearson"]:
+
+            # we obviously want to skip any keys that we aren't expecting
+            if key in results_exp_obj:
+
+                actual = results_obj[key]
+                expected = results_exp_obj[key]
+
+                # if this is a float, then we check with less precision (2 decimals);
+                # otherwise, we check to make sure things are matching exactly
+                if isinstance(expected, float):
+                    assert_almost_equal(actual, expected, places=2)
+                else:
+                    eq_(actual, expected)
 
 
 def setup():
@@ -86,7 +127,7 @@ def test_titanic_configs():
     Run all of the configuration files for the titanic example
     """
     for config_path in glob(join(_new_titanic_dir, 'titanic', '*.cfg')):
-        run_configuration(config_path, quiet=True)
+        run_configuration_and_check_outputs(config_path)
 
 
 def test_boston_configs():
@@ -94,7 +135,7 @@ def test_boston_configs():
     Run all of the configuration files for the boston example
     """
     for config_path in glob(join(_new_boston_dir, 'boston', '*.cfg')):
-        run_configuration(config_path, quiet=True)
+        run_configuration_and_check_outputs(config_path)
 
 
 def test_iris_configs():
@@ -102,4 +143,4 @@ def test_iris_configs():
     Run all of the configuration files for the iris example
     """
     for config_path in glob(join(_new_iris_dir, 'iris', '*.cfg')):
-        run_configuration(config_path, quiet=True)
+        run_configuration_and_check_outputs(config_path)
