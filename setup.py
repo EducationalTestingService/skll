b02244891aa2b7d8a08fe8c3d56775e7dc9c26ec
Nitin Madnani <nmadnani@ets.org>
2019-10-22 17:03:58 -0400

    Fix typo in `setup.py`.

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(name='skll',
                    'Operating System :: MacOS',
                    'Programming Language :: Python :: 3',
                    'Programming Language :: Python :: 3.6',
-                   'Programming Language :: Python :: 3.7'
-                   'Programming Language :: Python :: 3.8'
+                   'Programming Language :: Python :: 3.7',
+                   'Programming Language :: Python :: 3.8',
                    ],
       zip_safe=False)
