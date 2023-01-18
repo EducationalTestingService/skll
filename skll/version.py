beb27e2920c31724fcdb5a3abf86739c2ccda24f
Nitin Madnani <nmadnani@ets.org>
2023-01-18 14:47:53 -0500

    Update version number

--- skll/version.py
+++ skll/version.py
@@ -8,5 +8,5 @@ in one place. Based on the suggestion `here. <http://bit.ly/16LbuJF>`_
 :organization: ETS
 """
 
-__version__ = '3.1.0'
+__version__ = '3.2.0'
 VERSION = tuple(int(x) for x in __version__.split('.'))
