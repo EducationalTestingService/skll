dc233dd36f58985b4d0a274cae43a8de3bb5d6b7
Nitin Madnani <nmadnani@ets.org>
2023-06-08 19:45:25 -0400

    chore(types): improve `skll.types` module
    - Convert all custom types to `TypeAlias` objects.
    - Sort alphabetically.

--- skll/types.py
+++ skll/types.py
@@ -1,6 +1,6 @@
 # License: BSD 3 clause
 """
-Custom type annotations for readability.
+Custom type aliases for readability.
 
 :author: Nitin Madnani (nmadnani@ets.org)
 """
@@ -9,56 +9,69 @@ from __future__ import annotations
 from pathlib import Path
 from typing import TYPE_CHECKING, Any, Dict, Generator, Iterator, List, Optional, Tuple, Union
 
+import numpy as np
+from scipy.sparse import csr_matrix
+from typing_extensions import TypeAlias
+
 # conditional imports to avoid circular dependency problem
 if TYPE_CHECKING:
     import skll.data
     import skll.learner
 
-import numpy as np
-from scipy.sparse import csr_matrix
-
 # a class map that maps new labels (string)
 # to list of old labels (list of string)
-ClassMap = Dict[str, List[str]]
+ClassMap: TypeAlias = Dict[str, List[str]]
+
+# confusion matrix
+ConfusionMatrix: TypeAlias = List[List[int]]
 
 # list of feature dictionaries
-FeatureDict = Dict[str, Any]
-FeatureDictList = List[FeatureDict]
+FeatureDict: TypeAlias = Dict[str, Any]
+FeatureDictList: TypeAlias = List[FeatureDict]
+
+# an iterator over two featuresets, usually test and train
+FeaturesetIterator: TypeAlias = Iterator[Tuple["skll.data.FeatureSet", "skll.data.FeatureSet"]]
 
 # a mapping from example ID to fold ID;
 # the example ID may be a float or a str
-FoldMapping = Dict[Union[float, str], str]
+FoldMapping: TypeAlias = Dict[Union[float, str], str]
 
 # a float or a string; this is useful
 # for SKLL IDs that can be both
-IdType = Union[float, str]
+IdType: TypeAlias = Union[float, str]
+
+# generator over two index numpy arrays - usually train and test
+IndexIterator: TypeAlias = Generator[Tuple[np.ndarray, np.ndarray], None, None]
 
 # a float, int, or a string; this is useful
 # for SKLL labels that can be both
-LabelType = Union[float, int, str]
+LabelType: TypeAlias = Union[float, int, str]
+
+# learning curve sizes can either be a numpy array or a list of floats or ints
+LearningCurveSizes: TypeAlias = Union[
+    List[Union[float, int]],
+    np.ndarray,
+]
 
 # a generator that yields a three-tuple:
 # - an example ID (float or str)
 # - a label (int, float, or str)
 # - a feature dictionary
-FeatGenerator = Generator[Tuple[IdType, Optional[LabelType], FeatureDict], None, None]
+FeatGenerator: TypeAlias = Generator[Tuple[IdType, Optional[LabelType], FeatureDict], None, None]
 
 # a string path or Path object
-PathOrStr = Union[Path, str]
+PathOrStr: TypeAlias = Union[Path, str]
 
 # a sparse matrix for features
-SparseFeatureMatrix: csr_matrix = csr_matrix
+SparseFeatureMatrix: TypeAlias = csr_matrix
 
-# confusion matrix
-ConfusionMatrix = List[List[int]]
-
-# Learner evaluate task results 6-tuple:
+# Learner evaluate task results 5-tuple:
 # - confusion matrix (classifier) / None (regressor)
 # - accuracy (classifier) / None (regressor)
 # - dictionary of results
 # - score for the grid objective, None if no grid search
 # - dictionary of score for any additional metrics
-ComputeEvalMetricsResults = Tuple[
+ComputeEvalMetricsResults: TypeAlias = Tuple[
     Optional[ConfusionMatrix],
     Optional[float],
     Dict[LabelType, Any],
@@ -73,7 +86,7 @@ ComputeEvalMetricsResults = Tuple[
 # - model parameters dictionary
 # - score for the grid objective, None if no grid search
 # - dictionary of score for any additional metrics
-EvaluateTaskResults = Tuple[
+EvaluateTaskResults: TypeAlias = Tuple[
     Optional[ConfusionMatrix],
     Optional[float],
     Dict[LabelType, Any],
@@ -91,7 +104,7 @@ EvaluateTaskResults = Tuple[
 #   associated with each hyperparameter set combination.
 # - dictionary containing the test-fold number for each, None if not saved
 # - a list of learners, one for each fold, None if not saved
-CrossValidateTaskResults = Tuple[
+CrossValidateTaskResults: TypeAlias = Tuple[
     List[EvaluateTaskResults],
     List[float],
     List[Dict[str, Any]],
@@ -105,20 +118,8 @@ CrossValidateTaskResults = Tuple[
 # - the grid search scores for each fold.
 # - dictionary containing the test-fold number for each, None if not saved
 # - a list of learners, one for each fold, None if not saved
-VotingCrossValidateTaskResults = Tuple[
+VotingCrossValidateTaskResults: TypeAlias = Tuple[
     List[EvaluateTaskResults],
     Optional[FoldMapping],
     Optional[List["skll.learner.voting.VotingLearner"]],
 ]
-
-# learning curve sizes can either be a numpy array or a list of floats or ints
-LearningCurveSizes = Union[
-    List[Union[float, int]],
-    np.ndarray,
-]
-
-# an iterator over two featuresets, usually test and train
-FeaturesetIterator = Iterator[Tuple["skll.data.FeatureSet", "skll.data.FeatureSet"]]
-
-# generator over two index numpy arrays - usually train and test
-IndexIterator = Generator[Tuple[np.ndarray, np.ndarray], None, None]
