3ffdea6c66a1440d8f0029dd4727d32a7b039c7c
Tamar Lavee <tlavee@ets.org>
2024-02-22 10:31:18 -0500

    add python 3.12 in setup + copyright 2024

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
     zip_safe=False,
 )
